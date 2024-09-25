22476fd2c0d2ddafc5c27db194d7d86c2bb680ea
jaz <163017403+jazmmiin@users.noreply.github.com>
2024-09-25 19:57:39 -0300

    Add files via upload

---
+++ NuevoBoton.tsx
@@ -0,0 +1,36 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { StyleSheet, Image, Platform } from 'react-native';
+
+import { Collapsible } from '@/components/Collapsible';
+import { ExternalLink } from '@/components/ExternalLink';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function TabTwoScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#F7AF9D', dark: 'red' }}
+      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title"> holi </ThemedText>
+      </ThemedView>
+      <div>
+  <img src="./assets/images/michu.jpeg" alt="michu"></img>
+  </div>
+      </ParallaxScrollView>
+  );
+}
+      
+const styles = StyleSheet.create({
+  headerImage: {
+    color: 'pink',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+});
